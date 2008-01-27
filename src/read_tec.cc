48f69f6586629c2e760de5e79d0e22eed07ea9c0
esozer <esozer@0035eb8a-ee81-11dd-877d-0784077f618c>
2008-01-27 09:57:58 +0000

    - Parallel restart works

--- src/read_tec.cc
+++ src/read_tec.cc
@@ -9,7 +9,7 @@ using namespace std;
 
 extern Grid grid;
 
-void read_tec(string fileName, double &time) {
+void read_tec(string fileName, int global2local[], double &time) {
 	fstream file;
 	string data="";
 
@@ -21,15 +21,29 @@ void read_tec(string fileName, double &time) {
 
 	int lineCount;
 
-// Skip the node data
-	lineCount=grid.nodeCount*3/int (100);
+	// Skip the node data
+	lineCount=grid.globalNodeCount*3/int (100);
 	if ((grid.nodeCount*3) %int (100) !=0) lineCount++;
 	for (unsigned int n=0;n<lineCount;++n) getline(file,data,'\n');
 
-	for (unsigned int c=0;c<grid.cellCount;++c) file >> grid.cell[c].rho;
-	for (unsigned int i=0;i<3;++i) for (unsigned int c=0;c<grid.cellCount;++c) file >> grid.cell[c].v.comp[i];
-	for (unsigned int c=0;c<grid.cellCount;++c) file >> grid.cell[c].p;
-
+	double temp;
+	for (unsigned int c=0;c<grid.globalCellCount;++c) {
+		file >> temp;
+		if (global2local[c]>=0) grid.cell[global2local[c]].rho=temp;
+	}
+	
+	for (unsigned int i=0;i<3;++i) {
+		for (unsigned int c=0;c<grid.globalCellCount;++c) {
+			file >> temp;
+			if (global2local[c]>=0) grid.cell[global2local[c]].v.comp[i]=temp;
+		}
+	}
+	
+	for (unsigned int c=0;c<grid.globalCellCount;++c) {
+		file >> temp;
+		if (global2local[c]>=0) grid.cell[global2local[c]].p=temp;
+	}
+	
 	file.close();
 
 }
