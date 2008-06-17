1e6c22edf8c992aad17acba908f2f40886ee0fce
esozer <esozer@0035eb8a-ee81-11dd-877d-0784077f618c>
2008-06-17 12:54:39 +0000

    - More work towards implicit

--- src/jac.cc
+++ src/jac.cc
@@ -12,52 +12,72 @@ void RoeFlux(const double qL[], const double qR[], double flux[]);
 
 void jac(Mat impOP) {
 
-	double rhoL,rhoR,uNL,uNR,vTL,vTR,wTL,wTR, pL, pR;
-	Vec3D faceTangent1, faceTangent2;
-	double qL[5], qR[5], fluxNormal[5], flux[5], fluxPlus[5], delta[5];
-
+	double rhoL,rhoR,pL,pR,uL,vL,wL,uR,vR,wR;
+	Vec3D faceTangent1, faceTangent2, qVL, qVR, deltaV;
+	double qNL[5], qNR[5], fluxNormal[5], flux[5], fluxPlus[5], deltaL[5], deltaR[5];
+	
 	unsigned int parent,neighbor,f;
 
 	int row,col;
 	PetscScalar value;
+	
+	for (int i=0;i<5;++i) {
+		deltaL[i]=sqrt(std::numeric_limits<double>::epsilon());
+		deltaR[i]=sqrt(std::numeric_limits<double>::epsilon());
+	}
+	deltaV=0.;
 
-	for (int i=0;i<5;++i) delta[i]=0.;
-		
 	for (unsigned int f=0;f<grid.faceCount;++f) {
 			parent=grid.face[f].parent; neighbor=grid.face[f].neighbor;
 
 			if (grid.face[f].bc==-1) { // means a real internal face
-				
-			// Take 2 nodes of the face to find a vector in the face plane
-				faceTangent1= (grid.face[f].node(1)-grid.face[f].node(0));
-				faceTangent1/=fabs(faceTangent1);
-			// Cross the tangent vector with the normal vector to get the second tangent
-				faceTangent2= (grid.face[f].normal).cross(faceTangent1);
-				uNL=grid.cell[parent].v.dot(grid.face[f].normal);
-				vTL=grid.cell[parent].v.dot(faceTangent1);
-				wTL=grid.cell[parent].v.dot(faceTangent2);
+
 				rhoL=grid.cell[parent].rho;
 				pL=grid.cell[parent].p;
 
-				uNR=grid.cell[neighbor].v.dot(grid.face[f].normal);
-				vTR=grid.cell[neighbor].v.dot(faceTangent1);
-				wTR=grid.cell[neighbor].v.dot(faceTangent2);
 				rhoR=grid.cell[neighbor].rho;
 				pR=grid.cell[neighbor].p;
 				
-				qL[0]=rhoL;
-				qL[1]=qL[0] * uNL;
-				qL[2]=qL[0] * vTL;
-				qL[3]=qL[0] * wTL;
-				qL[4]=0.5*qL[0]* (uNL*uNL+vTL*vTL+wTL*wTL) +pL/ (Gamma - 1.);
+				// Take 2 nodes of the face to find a vector in the face plane
+				faceTangent1= (grid.face[f].node(1)-grid.face[f].node(0));
+				faceTangent1/=fabs(faceTangent1);	
+				// Cross the tangent vector with the normal vector to get the second tangent
+				faceTangent2= (grid.face[f].normal).cross(faceTangent1);
+		
+				qVL.comp[0]=rhoL * uL;
+				qVL.comp[1]=rhoL * vL;
+				qVL.comp[2]=rhoL * wL;
+				
+				qNL[0]=rhoL;
+				qNL[1]=qVL.dot(grid.face[f].normal);
+				qNL[2]=qVL.dot(faceTangent1);
+				qNL[3]=qVL.dot(faceTangent2);
+				qNL[4]=0.5*rhoL* (uL*uL+vL*vL+wL*wL) +pL/ (Gamma - 1.);
+				
+				qVR.comp[0]=rhoR * uR;
+				qVR.comp[1]=rhoR * vR;
+				qVR.comp[2]=rhoR * wR;
+				
+				qNR[0]=rhoR;
+				qNR[1]=qVR.dot(grid.face[f].normal);
+				qNR[2]=qVR.dot(faceTangent1);
+				qNR[3]=qVR.dot(faceTangent2);
+				qNR[4]=0.5*rhoR* (uR*uR+vR*vR+wR*wR) +pR/ (Gamma - 1.);
 
-				qR[0] = rhoR;
-				qR[1] = qR[0] * uNR;
-				qR[2] = qR[0] * vTR;
-				qR[3] = qR[0] * wTR;
-				qR[4] = 0.5*qR[0]* (uNR*uNR+vTR*vTR+wTR*wTR) +pR/ (Gamma - 1.);
-
-				RoeFlux(qL, qR, fluxNormal);
+				
+				deltaV=0.;deltaV.comp[0]=deltaL[0];
+				deltaL[1]=(qVL+deltaV).dot(grid.face[f].normal)-qNL[1];
+				deltaR[1]=(qVR+deltaV).dot(grid.face[f].normal)-qNR[1];
+								
+				deltaV=0.; deltaV.comp[1]=deltaL[0];
+				deltaL[2]=(qVL+deltaV).dot(faceTangent1)-qNL[2];
+				deltaR[2]=(qVR+deltaV).dot(faceTangent1)-qNR[2];
+				
+				deltaV=0.; deltaV.comp[2]=deltaL[0];
+				deltaL[3]=(qVL+deltaV).dot(faceTangent2)-qNL[3];
+				deltaR[3]=(qVR+deltaV).dot(faceTangent2)-qNR[3];
+				
+				RoeFlux(qNL, qNR, fluxNormal);
 
 				flux[0] = fluxNormal[0]*grid.face[f].area;
 				flux[1] = (fluxNormal[1]*grid.face[f].normal.comp[0]+fluxNormal[2]*faceTangent1.comp[0]+fluxNormal[3]*faceTangent2.comp[0]) * grid.face[f].area;
@@ -65,21 +85,13 @@ void jac(Mat impOP) {
 				flux[3] = (fluxNormal[1]*grid.face[f].normal.comp[2]+fluxNormal[2]*faceTangent1.comp[2]+fluxNormal[3]*faceTangent2.comp[2]) * grid.face[f].area;
 				flux[4] = fluxNormal[4]*grid.face[f].area;
 
-				//for (int i=0;i<5;++i) delta[i]=max(fabs(qL[i]*1.e-4),1.e-4);
-				//for (int i=0;i<5;++i) delta[i]=sqrt(1.e-12)*(1.+fabs(qL[i]));
-				for (int i=0;i<5;++i) delta[i]=sqrt(std::numeric_limits<double>::epsilon());
-								
 				for (int i=0;i<5;++i) {
 
-					qL[0]=rhoL;
-					qL[1]=qL[0] * uNL;
-					qL[2]=qL[0] * vTL;
-					qL[3]=qL[0] * wTL;
-					qL[4]=0.5*qL[0]* (uNL*uNL+vTL*vTL+wTL*wTL) +pL/ (Gamma - 1.);
+					qNL[i]+=deltaL[i];
 
-					qL[i]+=delta[i];
+					RoeFlux(qNL, qNR, fluxNormal);
 
-					RoeFlux(qL, qR, fluxNormal);
+					qNL[i]-=deltaL[i];
 
 					fluxPlus[0] = fluxNormal[0]*grid.face[f].area;
 					fluxPlus[1] = (fluxNormal[1]*grid.face[f].normal.comp[0]+fluxNormal[2]*faceTangent1.comp[0]+fluxNormal[3]*faceTangent2.comp[0]) * grid.face[f].area;
@@ -87,11 +99,11 @@ void jac(Mat impOP) {
 					fluxPlus[3] = (fluxNormal[1]*grid.face[f].normal.comp[2]+fluxNormal[2]*faceTangent1.comp[2]+fluxNormal[3]*faceTangent2.comp[2]) * grid.face[f].area;
 					fluxPlus[4] = fluxNormal[4]*grid.face[f].area;
 
-					//int j=i;
+					// i is the variable, j is the flux component
 					for (int j=0;j<5;++j) {
 						row=grid.cell[parent].globalId*5+j;
 						col=grid.cell[parent].globalId*5+i;
-						value=(fluxPlus[j]-flux[j])/delta[j];
+						value=(fluxPlus[j]-flux[j])/deltaL[i];
 						
 						//cout << fluxPlus[i] << "\t" << flux[i] << "\t" << delta[i] << "\t" << value << endl;
 						MatSetValues(impOP,1,&row,1,&col,&value,ADD_VALUES);
@@ -103,28 +115,14 @@ void jac(Mat impOP) {
 					} // for j
 
 				} // for i
-
-				qL[0]=rhoL;
-				qL[1]=qL[0] * uNL;
-				qL[2]=qL[0] * vTL;
-				qL[3]=qL[0] * wTL;
-				qL[4]=0.5*qL[0]* (uNL*uNL+vTL*vTL+wTL*wTL) +pL/ (Gamma - 1.);
-
-				//for (int i=0;i<5;++i) delta[i]=max(fabs(qR[i]*1.e-4),1.e-4);
-				//for (int i=0;i<5;++i) delta[i]=sqrt(1.e-12)*(1.+fabs(qR[i]));
-				for (int i=0;i<5;++i) delta[i]=sqrt(std::numeric_limits<double>::epsilon());
 								
 				for (int i=0;i<5;++i) {
 
-					qR[0] = rhoR;
-					qR[1] = qR[0] * uNR;
-					qR[2] = qR[0] * vTR;
-					qR[3] = qR[0] * wTR;
-					qR[4] = 0.5*qR[0]* (uNR*uNR+vTR*vTR+wTR*wTR) +pR/ (Gamma - 1.);
+					qNR[i]+=deltaR[i];
 
-					qR[i]+=delta[i];
+					RoeFlux(qNL, qNR, fluxNormal);
 
-					RoeFlux(qL, qR, fluxNormal);
+					qNR[i]-=deltaR[i];
 
 					fluxPlus[0] = fluxNormal[0]*grid.face[f].area;
 					fluxPlus[1] = (fluxNormal[1]*grid.face[f].normal.comp[0]+fluxNormal[2]*faceTangent1.comp[0]+fluxNormal[3]*faceTangent2.comp[0]) * grid.face[f].area;
@@ -132,11 +130,10 @@ void jac(Mat impOP) {
 					fluxPlus[3] = (fluxNormal[1]*grid.face[f].normal.comp[2]+fluxNormal[2]*faceTangent1.comp[2]+fluxNormal[3]*faceTangent2.comp[2]) * grid.face[f].area;
 					fluxPlus[4] = fluxNormal[4]*grid.face[f].area;
 
-					//int j=i;
 					for (int j=0;j<5;++j) {
 						row=grid.cell[neighbor].globalId*5+j;
 						col=grid.cell[neighbor].globalId*5+i;
-						value=(flux[j]-fluxPlus[j])/delta[j];
+						value=(flux[j]-fluxPlus[j])/deltaR[i];
 						//cout << fluxPlus[i] << "\t" << flux[i] << "\t" << delta[i] << "\t" << value << endl;
 						MatSetValues(impOP,1,&row,1,&col,&value,ADD_VALUES);
 						row=grid.cell[parent].globalId*5+j;
