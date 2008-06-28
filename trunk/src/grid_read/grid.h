58c4286ef178488f1ac222af0c0694cc6e8aa855
esozer <esozer@0035eb8a-ee81-11dd-877d-0784077f618c>
2008-06-28 19:59:18 +0000

    - more examples added

---
+++ trunk/src/grid_read/grid.h
@@ -0,0 +1,91 @@
+#ifndef GRID_H
+#define GRID_H
+
+/*
+  Classes for reading and storing grid information
+*/
+
+#include <cgnslib.h>
+#include <vector>
+#include <mpi.h>
+#include "vec3d.h"
+#include "sparse.h"
+using namespace std;
+
+
+class Node : public Vec3D {
+public:
+	unsigned int id, globalId;
+	std::vector<int> cells;
+	std::vector<int> ghosts;
+	std::map<int,double> average;
+	Node(double x=0., double y=0., double z=0.);
+};
+
+class Face {
+public:
+	int bc;
+	unsigned int id;
+	int parent,neighbor;
+	unsigned int nodeCount;
+	Vec3D centroid;
+	Vec3D normal;
+	std::map<int,double> average;
+	double area;
+	std::vector<int> nodes;
+	Node& node(int n);
+};
+
+class Cell {
+public:
+	unsigned int nodeCount,faceCount,neighborCellCount,ghostCount,globalId,globalCellCount;
+	ElementType_t type;
+	double volume,lengthScale;
+	Vec3D centroid;
+	std::vector<int> nodes;
+	std::vector<int> faces;
+	std::vector<int> neighborCells;
+	std::vector<int> ghosts;
+	double rho;
+	Vec3D v,grad[5],limited_grad[5];
+	std::map<int,Vec3D> gradMap;
+	double p;
+	double flux[5];
+	Cell(void);
+	int Construct(const ElementType_t elemType,unsigned int nodeList[]);
+	bool HaveNodes(unsigned int &nodelistsize, unsigned int nodelist[]) ;
+	Node& node(int n);
+	Face& face(int f);
+};
+
+class Ghost {
+public:
+	unsigned int partition;
+	unsigned int globalId;
+	std::vector<unsigned int> cells;
+	double rho;
+	Vec3D v,centroid,grad[5],limited_grad[5];
+	double p;
+};
+
+class Grid {
+public:
+	string fileName;
+	unsigned int nodeCount,cellCount,faceCount;
+	unsigned int globalNodeCount,globalCellCount,globalFaceCount,ghostCount;
+	std::vector<Node> node;
+	std::vector<Face> face;
+	std::vector<Cell> cell;
+	std::vector<Ghost> ghost;
+	int ReadCGNS();
+	Grid();
+	int read(string);
+	int face_exists(int &parentCell);
+	void nodeAverages();
+	void faceAverages();
+	void gradMaps();
+	void gradients();
+	void limit_gradients(string limiter, double sharpeningFactor);
+};
+
+#endif
