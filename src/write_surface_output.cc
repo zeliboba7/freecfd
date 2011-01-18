7f15957f4c5964d6b54fef411048ce63c6c1553e
emresozer <emresozer@0035eb8a-ee81-11dd-877d-0784077f618c>
2011-01-18 23:43:29 +0000

    - A fix in face interpolation

--- src/write_surface_output.cc
+++ src/write_surface_output.cc
@@ -187,6 +187,8 @@ void write_surface_tec_var(int ov,int i,int b) {
 	
 	if (varList[ov]=="p") {
 		for (int bf=0;bf<grid[gid].boundaryFaces[b].size();++bf) {
+			//int parent=grid[gid].face[grid[gid].boundaryFaces[b][bf]].parent;
+			//file << ns[gid].p.cell(parent);
 			file << ns[gid].p.face(grid[gid].boundaryFaces[b][bf]);
 			if ((bf+1)%10==0) file << "\n";
 			else file << "\t";
